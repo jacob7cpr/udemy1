85e02e80a912f0c8930f22d3673057d378de1442
Jacob Cooper <lilnik454@gmail.com>
2020-04-06 15:41:41 +0300

    Add Superuser And TDD

--- app/core/models.py
+++ app/core/models.py
@@ -7,13 +7,21 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Create and save a new user """
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users most have an emaila ddress')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
 
+    def create_superuser(self, email, password):
+        """ Create and save Super User """
+        user = self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ Custom user model that suuport email """
--- app/core/test/test_model.py
+++ app/core/test/test_model.py
@@ -15,4 +15,25 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
+
+
+    def test_new_user_email_normalized(self):
+        """ Test email for new user """
+        email = 'liilhkj@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'TestPas1223')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ test creating user with no valid emial """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
+
+    def test_create_new_super_user(self):
+        """ test creating new user"""
+        user = get_user_model().objects.create_superuser(
+            'test@lil.com'
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
